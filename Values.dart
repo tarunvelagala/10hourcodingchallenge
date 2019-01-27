fe155887f01133d4ff078f235a924486231d03f4
Tarun Kumar <tarunvelagala80@gmail.com>
2019-01-27 11:13:06 +0530

    Create Values.dart

---
+++ Values.dart
@@ -0,0 +1,12 @@
+main(){
+  // printing strings
+  print("10 hour"+ 'coding challenge');
+  print("Dart Learning");
+  
+  /* Different datatypes exploration */ // comment lines(multiple lines)
+  print("1+1=${1+1}");
+  print("7.0/3.0 =${7.0/3.0}");
+  print(true && false);
+  print(false || true);
+  print(!true);
+}
