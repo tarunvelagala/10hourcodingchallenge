de710f813e444ef37a19cee647d08108dee96662
Tarun Kumar <tarunvelagala80@gmail.com>
2019-01-27 12:35:27 +0530

    Create loops.dart

---
+++ loops.dart
@@ -0,0 +1,36 @@
+void main() { 
+   outerloop: // This is the label name 
+   
+   for (var i = 0; i < 5; i++) { 
+      print("Innerloop: ${i}"); 
+      innerloop: 
+      
+      for (var j = 0; j < 5; j++) { 
+         if (j > 3 ) break ; 
+         
+         // Quit the innermost loop 
+         if (i == 2) break innerloop; 
+         
+         // Do the same thing 
+         if (i == 4) break outerloop; 
+         
+         // Quit the outer loop 
+         print("Innerloop: ${j}"); 
+      } 
+   } 
+}
+// label with continue
+void main() { 
+   outerloop: // This is the label name 
+   
+   for (var i = 0; i < 3; i++) { 
+      print("Outerloop:${i}"); 
+      
+      for (var j = 0; j < 5; j++) { 
+         if (j == 3){ 
+            continue outerloop; 
+         } 
+         print("Innerloop:${j}"); 
+      } 
+   } 
+}
