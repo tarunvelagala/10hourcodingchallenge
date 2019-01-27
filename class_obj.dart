e390cc2d489e1807b554821e3f61ba652b225ae0
Tarun Kumar <tarunvelagala80@gmail.com>
2019-01-27 10:29:10 +0530

    Update class_obj.dart

--- class_obj.dart
+++ class_obj.dart
@@ -1,9 +1,19 @@
-class TestClass{
-  void display(){
-    print('My Class TestClass');
+import 'dart:math';
+class Triangle{
+  int a,b,c;
+  double getArea(){
+    double s = (a+b+c)/2.0;
+    return pow((s*(s-a)*(s-b)*(s-c)),.5);
+}
+  double getPerimeter(){
+    return (a+b+c)/2.0;
   }
 }
 void main(){
-  TestClass c = new TestClass();
-  c.display();
+  Triangle t = new Triangle();
+  t.a = 2;
+  t.b = 5;
+  t.c = 6;
+  print(t.getArea());
+  print(t.getPerimeter());
 }
