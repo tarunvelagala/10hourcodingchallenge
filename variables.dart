86474ed3ba32d41b175cca0c4446f8845f9078da
Tarun Kumar <tarunvelagala80@gmail.com>
2019-01-27 11:17:20 +0530

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,22 @@
+main() {
+  // 'var' declares a variable.  dartanalyzer infers the type.
+  var a = "initial";
+  print(a);
+
+  // The type can also be declared:
+  num b = 42;
+  print(b);
+
+  // final variables cannot be changed once declared
+  final num c = 99;
+  print(c);
+
+  // const variables are compile-time constants
+  const double d = 44.00;
+  print(d);
+  
+  // dynamic type variables, they can be changed to any other data type 
+  dynamic s = 'Tarun';
+  s = 1642;
+  print(s); 
+}
